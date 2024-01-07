6efb60fa0b1b8f176477aa9c7cd7a2e2c4af8ce7
jokerwyt <914554688@qq.com>
2024-01-07 20:12:23 +0800

    test client upd

--- test_client.py
+++ test_client.py
@@ -4,12 +4,19 @@ from scapy.all import Ether, IP, TCP, sendp
 # veth2-1 f2:3e:82:54:05:c5
 # veth1-2 b6:9d:50:bf:43:b2
 
-ethernet_frame = Ether(src='f2:3e:82:54:05:c5', dst='b6:9d:50:bf:43:b2')
+# ethernet_frame = Ether(src='f2:3e:82:54:05:c5', dst='b6:9d:50:bf:43:b2', type=)
 
-ip_packet = IP(src='111.111.111.111', dst='222.222.222.222')
+# ip_packet = IP(src='111.111.111.111', dst='222.222.222.222')
 
-tcp_packet = TCP(sport=1234, dport=80, flags='S', seq=1000)
+# tcp_packet = TCP(sport=1234, dport=80, flags='S', seq=1000)
 
-packet = ethernet_frame / ip_packet / tcp_packet
+# packet = ethernet_frame / ip_packet / tcp_packet
 
-sendp(packet, iface='veth2-1')
+# sendp(packet, iface='veth2-1')
+
+
+# send 100 dummpy ethernet frames, with payload 'hello world'
+for i in range(100):
+    ethernet_frame = Ether(src='f2:3e:82:54:05:c5', dst='b6:9d:50:bf:43:b2')
+    packet = ethernet_frame / 'hello world'
+    sendp(packet, iface='veth2-1')
